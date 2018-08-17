2ad07ab800847c096147d3cf75bb475e5ce4c4ce
Nidhi Mittal <31856850+nidhimittal2@users.noreply.github.com>
2018-08-17 18:31:22 +0530

    Create Day6-Jsdates.js

---
+++ Day6-Jsdates.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    var d =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayName;
+    var date = new Date(dateString)
+    dayName = date.getDay();
+    return d[dayName];
+}
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
